a5019578d85825cc0465fa2b75d2eaead6f69144
unknown <danut.brianda@gmail.com>
2022-10-07 12:31:07 +0200

    Comit 1

---
+++ Danut.py
@@ -0,0 +1,12 @@
+from numpy import sqrt
+
+
+class Point3D:
+
+    
+
+    #Commit 1: __init__(constructor), setters, getters and __str__ (tostring)
+    def_init_(self,x,y,z):
+        self.x=x
+        self.y=y
+        self.z=z
