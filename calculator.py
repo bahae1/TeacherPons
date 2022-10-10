5d193150d7bcc089fe3a385c160d462bf3d52ad1
Albertougr <72387745+Albertougr@users.noreply.github.com>
2022-10-10 16:13:36 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -15,3 +15,22 @@ class Calculadora():
     return x+y
 
 a=Calculadora("basica")
+
+
+
+class circulo():
+	def __init__(self,radio):
+		self.radio=radio
+	
+	@property
+	def radio(self):
+		print("Estoy dando el radio")
+		return self._radio	
+
+	@radio.setter
+	def radio(self,radio):
+		if radio>=0:
+			self._radio = radio
+		else:
+			raise ValueError("Radio positivo")
+			self._radio=0
