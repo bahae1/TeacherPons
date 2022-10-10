0d32cf127337b434b3d870b412dca7d57b68ee43
davidteitor <92325973+davidteitor@users.noreply.github.com>
2022-10-10 16:11:54 +0200

    Update droide.py

--- droide.py
+++ droide.py
@@ -11,3 +11,23 @@ class Droid:
     def name(self, name):
         print('inside the setter')
         self.hidden_name = name
+        
+        
+def addere(a, b):
+    '''Sum of input values'''
+    return a + b
+
+
+def minuas(a, b):
+    '''Substract of input values'''
+    return a - b
+
+
+def pullulate(a, b):
+    '''Product of input values'''
+    return a * b
+
+
+def partitus(a, b):
+    '''Division of input values'''
+    return a / b
