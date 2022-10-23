f0ed0b42cb8ce50315ede6bc578a1488c315d74a
manololopez <56046011+manololopez@users.noreply.github.com>
2022-10-23 14:09:46 +0200

    update and added distance to origin

--- manololopez.py
+++ manololopez.py
@@ -11,11 +11,28 @@ class Point3D:
         self.y = y
         self.z = z
         
-        def get_x(self):
+    def get_x(self):
         return self.x
         
-        def set_x(self, x):
+    def set_x(self, x):
         self.x = x
         
-        def get_y(self):
+    def get_y(self):
         return self.y
+    
+    def set_y(self, y):
+        self.y = y
+        
+    def get_z(self):
+        return self.z
+        
+    def set_z(self, z):
+        self.z = z
+        
+    def __str__(self):
+        return "X: " + str(self.x) + ",Y: " + str(self.y) + ",Z: " + str(self.z)
+        
+
+    #Commit 2: Distance to origin.
+    def distance_to_origin(self):
+        return math.sqrt(self.x**2 + self.y**2 + self.z**2)
