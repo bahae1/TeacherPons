a1343ce1362eb8516aaab6196300ec6d6ed48e73
David55bb <116066707+David55bb@users.noreply.github.com>
2022-10-23 14:59:46 +0200

    Update davidnew.py

--- davidnew.py
+++ davidnew.py
@@ -31,3 +31,18 @@ class Point3D:
     	
     def __str__(self):
     	return "Valores: X: " + str(self.get_X()) + ", Y: " + str(self.get_Y()) + ", Z: " + str(self.get_Z())
+
+    
+    
+    class circulo():
+        def __init__(self,radio):
+            self.set_radio(radio)
+        def set_radio(self,radio):
+            if radio>=0:
+                self._radio = radio
+            else:
+                raise ValueError("Radio positivo")
+                self._radio=0
+        def get_radio(self):
+            print("Estoy dando el radio")
+            return self._radio
