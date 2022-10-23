141ce537f5ab5162abb761fd0233fbb776ee646f
cryptofury7 <116491817+cryptofury7@users.noreply.github.com>
2022-10-23 14:59:05 +0200

    Update cryptofury7.py

--- cryptofury7.py
+++ cryptofury7.py
@@ -32,6 +32,23 @@ class Complex(object):
 
     def __ne__(self, other):
         return not self.__eq__(other)
+    
+    class Polynomial(object):
+    def __init__(self, coefficients):
+        self.coeff = coefficients
+
+    def __call__(self, x):
+        return sum([c*x**i for i, c in enumerate(self.coeff)])
+
+    def __add__(self, other):
+        maxlength = max(len(self), len(other))
+        # Extend both lists with zeros to this maxlength
+        self.coeff += [0]*(maxlength - len(self.coeff))
+        other.coeff += [0]*(maxlength - len(other.coeff))
+        result_coeff = self.coeff
+        for i in range(maxlength):
+            result_coeff[i] += other.coeff[i]
+        return Polynomial(result_coeff)
 
     def __str__(self):
         return '(%g, %g)' % (self.real, self.imag)
