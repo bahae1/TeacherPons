9948eaf04eed439e8aa5b22d20928d03455024e3
albduranlopezhd <116491943+albduranlopezhd@users.noreply.github.com>
2022-10-23 15:09:10 +0200

    Update lopezhd.py

--- lopezhd.py
+++ lopezhd.py
@@ -73,6 +73,20 @@ class Point3D:
                 minimo = distance
         return cercano
 
+class Reverse:
+    """Iterator for looping over a sequence backwards."""
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
 
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index = self.index - 1
+        return self.data[self.index]
+    
 if __name__ == "__main__":
     pass
