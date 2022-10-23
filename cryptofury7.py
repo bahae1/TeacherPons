79e218468715054c5753b7ae5d33bc6109d5e5e3
cryptofury7 <116491817+cryptofury7@users.noreply.github.com>
2022-10-23 14:16:14 +0200

    update cryptofury7 div, mul, abs complex

--- cryptofury7.py
+++ cryptofury7.py
@@ -10,3 +10,16 @@ class Complex(object):
     def __sub__(self, other):
         return Complex(self.real - other.real,
                        self.imag - other.imag)
+
+     def __mul__(self, other):
+        return Complex(self.real*other.real - self.imag*other.imag,
+                       self.imag*other.real + self.real*other.imag)
+
+    def __div__(self, other):
+        sr, si, or, oi = self.real, self.imag, \ 
+                         other.real, other.imag # short forms
+        r = float(or**2 + oi**2)
+        return Complex((sr*or+si*oi)/r, (si*or-sr*oi)/r)
+    
+    def __abs__(self):
+        return sqrt(self.real**2 + self.imag**2)
