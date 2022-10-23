6359270a4dd76ee6639fc1cb28a321b7e55780d7
albduranlopezhd <116491943+albduranlopezhd@users.noreply.github.com>
2022-10-23 13:16:15 +0200

    Create lopezhd.py

---
+++ lopezhd.py
@@ -0,0 +1,30 @@
+#from numpy import sqrt
+
+import math
+class Point3D:
+
+    #A point is defined by 3 coordinates
+    #Commit 1: __init__(constructor), setters, getters and __str__ (tostring)
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    
+    def get_X (self):
+    	return self.x
+    	
+    def set_X (self,x):
+    	self.x = x
+    
+    def get_Y (self):
+    	return self.y
+    	
+    def set_Y (self,y):
+    	self.y = y
+    	
+    def get_Z (self):
+    	return self.z
+    	
+    def set_Z (self,z):
+    	self.z = z
